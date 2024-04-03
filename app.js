03164c191035c15c717eba3ec21c0a5babe946ed
justwana-fly <nicolaibarbarasa@gmail.com>
2024-04-03 18:45:34 +0200

    aggiunto i contatti dell`array contacts per adesso img e nome

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+// Importiamo l'array dei contatti dal file script.js
 import { contacts } from "./script.js";
-//console.log(contacts)
+
+// Creiamo un'applicazione Vue
+const app = Vue.createApp({
+    // Definiamo la funzione data() per gestire i dati dell'applicazione
+    data() {
+        return {
+            // Inseriamo l'array dei contatti all'interno dei dati dell'applicazione
+            contacts: contacts
+        };
+    }
+});
+app.mount('#app');
